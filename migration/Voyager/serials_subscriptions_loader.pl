170f27bbd44385af3b81c0bb9fce45ce6536a426
Matti Lassila <matti.lassila@gmail.com>
2015-11-11 11:12:21 +0200

    Improved serials subscrions loader. Better logging, more imported subfields. Still very rough, currently used only for creating Koha testing instance.

--- migration/Voyager/serials_subscriptions_loader.pl
+++ migration/Voyager/serials_subscriptions_loader.pl
@@ -100,7 +100,6 @@ if ($bib_map_filename ne $NULL_STRING) {
    }
    close $map_file;
 }
-
 print "Reading MFHD records...\n";
 my %mfhd_data;
 if ($mfhd_in_filename ne $NULL_STRING) {
@@ -119,22 +118,57 @@ MFHD:
       my $fld001 = $record->field('001');
       next MFHD unless ($fld001);
       my $mfhd_number = $fld001->data();
+      my $location = $record->subfield('852','b');
       my $branch = $record->subfield('852','b');
       if ($static_branch ne $NULL_STRING) {
          $branch = $static_branch;
       }
+      print "Kokoelma: $location\n";
       next MFHD unless ($branch);
       if (defined $branch_map{$branch}) {
          $branch = $branch_map{$branch};
       }
-      my $holdings = $NULL_STRING;
+      print "Kirjasto: $branch\n";
+      my $callnumber = $NULL_STRING;
+      $callnumber = $record->subfield('852','h');
+      my $holdings = '';
+      my $missings = $NULL_STRING;
+      my $coverage = $NULL_STRING;
+      my $subscription_note = 'Migrated from Voyager. ';
+
+MFHD_994:
+      foreach my $field($record->field('994')) {
+         # next MFHD_863 if ($field->indicator(2) ne '0');
+         $subscription_note .= $field->subfield('a') . ' ';
+         next MFHD_994;
+      }
+
+
+
 MFHD_863:
       foreach my $field($record->field('863')) {
-         next MFHD_863 if ($field->indicator(2) ne '0');
+         # next MFHD_863 if ($field->indicator(2) ne '0');
          $holdings .= $field->subfield('a') . ' ';
+         $missings .= $field->subfield('z') . ' ';
+         next MFHD_863;
       } 
+MFHD_866:
+      foreach my $field($record->field('866')) {
+         # next MFHD_866 if ($field->indicator(2) ne '0');
+         $coverage .= $field->subfield('a') . ' ';
+         $coverage .= $field->subfield('z') . ' ';
+         next MFHD_866;        
+      }
+      print "Holdings:$holdings\n";
+
+      $mfhd_data{$mfhd_number}{'coverage'} = $coverage;       
+      $mfhd_data{$mfhd_number}{'missings'} = $missings;
+      $mfhd_data{$mfhd_number}{'callnumber'} = $callnumber;
+      $mfhd_data{$mfhd_number}{'location'} = $location;
       $mfhd_data{$mfhd_number}{'branch'} = $branch;
       $mfhd_data{$mfhd_number}{'holdings'} = $holdings;
+      $mfhd_data{$mfhd_number}{'subscription_note'} = $subscription_note;   
+
    }
 }
 
@@ -142,7 +176,8 @@ print "Reading subscription records...\n";
 my %sub_start_date;
 if ($subs_in_filename ne $NULL_STRING) {
    my $csv = Text::CSV_XS->new();
-   open my $subs_file,'<',$subs_in_filename;
+   
+open my $subs_file,'<:utf8',$subs_in_filename;
    $csv->column_names($csv->getline($subs_file));
 SUBS:
    while (my $line = $csv->getline_hr($subs_file)) {
@@ -155,7 +190,7 @@ print "Reading component records...\n";
 my %component_map;
 if ($component_in_filename ne $NULL_STRING) {
    my $csv = Text::CSV_XS->new({binary => 1});
-   open my $component_file,'<',$component_in_filename;
+   open my $component_file,'<:utf8',$component_in_filename;
    $csv->column_names($csv->getline($component_file));
 COMPONENT:
    while (my $line = $csv->getline_hr($component_file)) {
@@ -170,13 +205,13 @@ my $problem_2=0;
 my $dbh=C4::Context->dbh();
 my $sub_insert_sth = $dbh->prepare("INSERT INTO subscription
                                     (librarian,     branchcode, biblionumber,   notes, status,
-                                     internalnotes, location,   aqbooksellerid, startdate)
-                                    VALUES (?,?,?,?,?,?,?,?,?)");
+                                     internalnotes, location,   aqbooksellerid, startdate, callnumber)
+                                    VALUES (?,?,?,?,?,?,?,?,?,?)");
 my $hist_insert_sth = $dbh->prepare("INSERT INTO subscriptionhistory
-                                     (biblionumber, subscriptionid, recievedlist,librariannote)
-                                     VALUES (?,?,?,?)");
-my $csv=Text::CSV_XS->new({ binary => 1 });
-open my $input_file,'<',$input_filename;
+                                     (biblionumber, subscriptionid, recievedlist,librariannote,opacnote,missinglist)
+                                     VALUES (?,?,?,?,?,?)");
+my $csv=Text::CSV_XS->new({ binary => 1,sep_char =>"\t" });
+open my $input_file,'<:utf8',$input_filename;
 open my $output_file,'>',$output_filename;
 $csv->column_names($csv->getline($input_file));
 LINE:
@@ -187,22 +222,43 @@ while (my $line=$csv->getline_hr($input_file)) {
 
    my $biblionumber=$bib_map{$line->{BIB_ID}};
    if (!$biblionumber) {
-      print "Biblio not found!\n";
+      print "Biblio not found! $line->{BIB_ID}\n";
       $problem++;
       next LINE;
    }
    my $branchcode=$mfhd_data{$line->{MFHD_ID}}{'branch'};
-   my $holdings = $mfhd_data{$line->{MFHD_ID}}{'holdings'};
+   if (!exists $mfhd_data{$line->{MFHD_ID}}{'branch'}) {
+       $branchcode = "UNKNOWN";
+       print "Lopullinen pakoitettu sijaintikoodi $branchcode\n";
+  }   
+
+print "Lopullinen sijaintikoodi $branchcode\n";
+my $holdings = $NULL_STRING;
+  if (defined $mfhd_data{$line->{MFHD_ID}}{'holdings'}) {
+         $holdings = $mfhd_data{$line->{MFHD_ID}}{'holdings'};
+      } else {
+	$holdings = "Tarkista data";
+	}
+   my $coverage =  $NULL_STRING;
+  if (defined $mfhd_data{$line->{MFHD_ID}}{'coverage'}) {
+         $coverage = $mfhd_data{$line->{MFHD_ID}}{'coverage'};
+      } 
+ 
+   my $missings = $mfhd_data{$line->{MFHD_ID}}{'missings'};
+   my $callnumber = $mfhd_data{$line->{MFHD_ID}}{'callnumber'};
+   my $location = $mfhd_data{$line->{MFHD_ID}}{'location'};
    my $internalnote = $line->{NOTE};
+   my $subscription_note = 'Migrated from Voyager. '.$mfhd_data{$line->{MFHD_ID}}{'subscription_note'};
+   print "Holding-tiedot $holdings\n";
+   print "Huomautus $internalnote\n";
    next LINE if (!exists $component_map{ $line->{COMPONENT_ID} });
    my $startdate    = $sub_start_date{ $component_map{ $line->{COMPONENT_ID}}}; 
-
    if ($doo_eet) {
       $sub_insert_sth->execute('0', $branchcode, $biblionumber, undef, 1,
-                               $internalnote, undef, undef, $startdate);
+                               $internalnote, $location, undef, $startdate,$callnumber);
       my $subscription_id = $dbh->last_insert_id(undef, undef, undef, undef);
       if ($subscription_id) {
-         $hist_insert_sth->execute($biblionumber, $subscription_id, $holdings, "Migrated from Voyager");
+         $hist_insert_sth->execute($biblionumber, $subscription_id, $holdings, $subscription_note,$coverage,$missings);
          delete $component_map{ $line->{COMPONENT_ID}};
          print {$output_file} "$subscription_id,$line->{COMPONENT_ID}\n";
       }
@@ -239,12 +295,7 @@ sub _process_date {
    my $datein = shift;
    return undef if !$datein;
    return undef if $datein eq "";
-   my ($month,$day,$year) = $datein =~ /(\d+).(\d+).(\d\d\d\d)/;
-   if ($month && $day && $year) {
-   return sprintf "%4d-%02d-%02d",$year,$month,$day;
-   }
-   else {
-      return undef;
-   }
+   return $datein;
+
 }
 
