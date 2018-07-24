e711e9b807b84b7e3b0f8788e9d580ff6d16c4f0
Olli-Antti Kivilahti <olli-antti.kivilahti@helsinki.fi>
2018-07-24 16:38:52 +0300

    Loader - Deploy code while executing the Load-phase

--- loader/transformerToKoha_loadDataViaSSHJumpHosts.sh
+++ loader/transformerToKoha_loadDataViaSSHJumpHosts.sh
@@ -8,9 +8,13 @@
 ## This is an example implementation of the data extraction pipeline automation.
 ## You must write your own which will work in your environment.
 
-KOHA_HOST="koha-ci-jyu"
-KOHA_HOME="/home/koha/Koha"
+KOHA_HOST="koha-host-server"
 KOHA_LOAD_WORKING_DIR="/home/koha/KohaMigration"
+KOHA_LOADER_DIR="/home/koha/KohaLoader"
+
+#Deploy the loader program
+scp -r loader/KohaLoader $KOHA_HOST:$KOHA_LOADER_DIR
+test $? != 0 && echo "Uploading the Koha Loader failed!" && exit 9
 
 #Copy the loadable files in
 cd ~/MMT-Voyager && tar -czf kohaData.tar.gz KohaImports
@@ -19,12 +23,12 @@ test $? != 0 && echo "Packing Koha data failed!" && exit 10
 scp -r   ~/MMT-Voyager/kohaData.tar.gz $KOHA_HOST:/home/koha/
 test $? != 0 && echo "Uploading Koha data failed!" && exit 11
 
-ssh $KOHA_HOST "cd /home/koha && tar -xzf /home/koha/kohaData.tar.gz"
+ssh $KOHA_HOST "cd /home/koha && tar -xzf /home/koha/kohaData.tar.gz && rm /home/koha/kohaData.tar.gz"
 test $? != 0 && echo "Unpacking Koha data remotely failed!" && exit 12
 
 ssh $KOHA_HOST "mkdir -p $KOHA_LOAD_WORKING_DIR && chown koha:koha $KOHA_LOAD_WORKING_DIR"
 test $? != 0 && echo "Creating load-phase working dir remotely failed!" && exit 13
 
 #Start loading
-ssh $KOHA_HOST "$KOHA_HOME/misc/migration_tools/load.sh ~/KohaImports"
+ssh $KOHA_HOST "$KOHA_LOADER_DIR/load.sh /home/koha/KohaImports"
 test $? != 0 && echo "Loading Koha data failed!" && exit 14
