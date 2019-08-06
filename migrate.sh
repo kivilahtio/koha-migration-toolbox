b7588043bb17688cfb4c7e54a08d14a14c6df03a
Olli-Antti Kivilahti <olli-antti.kivilahti@hypernova.fi>
2019-08-06 20:41:55 +0300

    main migrate.sh is a bit more chatty about it's progress

--- migrate.sh
+++ migrate.sh
@@ -18,34 +18,57 @@ test -z "$SOURCE_SYSTEM" && echo "'sourceSystemType' is unknown. Couldn't parse 
 LOG_DIR="$MMT_HOME/logs"
 
 if [[ "$SOURCE_SYSTEM" =~ "Voyager" ]]; then
+  echo "--extract"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --extract         &> $LOG_DIR/01-extract.log
+  echo "--biblios"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --biblios         &> $LOG_DIR/02-biblios.log
+  echo "--holdings"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --holdings        &> $LOG_DIR/03-holdings.log
+  echo "--items"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --items           &> $LOG_DIR/04-items.log
+  echo "--patrons"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --patrons         &> $LOG_DIR/05-patrons.log
+  echo "--issues"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --issues          &> $LOG_DIR/06-issues.log
+  echo "--fines"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --fines           &> $LOG_DIR/07-fines.log
+  echo "--reserves"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --reserves        &> $LOG_DIR/08-reserves.log
+  echo "--serials"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --serials         &> $LOG_DIR/09-serials.log
+  echo "--subscriptions"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --subscriptions   &> $LOG_DIR/10-subscriptions.log
+  echo "--branchtransfers"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --branchtransfers &> $LOG_DIR/11-branchtransfers.log
+  echo "--load"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --load            &> $LOG_DIR/12-load.log
 fi
 
 if [[ "$SOURCE_SYSTEM" =~ "PrettyLib" ]]; then
+  echo "--biblios"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --biblios         &> $LOG_DIR/01-biblios.log
+  echo "--items"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --items           &> $LOG_DIR/02-items.log
+  echo "--patrons"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --patrons         &> $LOG_DIR/03-patrons.log
+  echo "--issues"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --issues          &> $LOG_DIR/04-issues.log
+  echo "--fines"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --fines           &> $LOG_DIR/05-fines.log
+  echo "--load"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --load            &> $LOG_DIR/10-load.log
 fi
 
 if [[ "$SOURCE_SYSTEM" =~ "PrettyCirc" ]]; then
+  echo "--biblios"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --biblios         &> $LOG_DIR/01-biblios.log
+  echo "--items"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --items           &> $LOG_DIR/02-items.log
+  echo "--subscriptions"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --subscriptions   &> $LOG_DIR/03-subscriptions.log
+  echo "--serials"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --serials         &> $LOG_DIR/04-serials.log
+  echo "--load"
   MMT_HOME="$MMT_HOME" perl -Itransformer transformer/transform.pl --load            &> $LOG_DIR/10-load.log
 fi
 
