5ac05ebd8ab4db46bc2d93e411a9ef7ae20148cc
Olli-Antti Kivilahti <olli-antti.kivilahti@hypernova.fi>
2019-10-27 18:06:04 +0200

    JNSKons - Custom item handling updated to passthrough the mapped value by default.

--- transformer/MMT/TranslationTable/ItemTypes.pm
+++ transformer/MMT/TranslationTable/ItemTypes.pm
@@ -41,22 +41,34 @@ JNSKonsa picks the itemtype from the acquisitions number prefix.
 my $K = 'KONVERSIO';
 sub JNSKonsa_itypes($s, $kohaObject, $prettyObject, $builder, $originalValue, $tableParams, $transParams) {
   my $an = $prettyObject->{AcqNum};
+  my $it;
   if ($an) {
 
-    if    ($an =~ /^nu/i)   { return 'NU' } # Nuotti
-    elsif ($an =~ /^cd/i)   { return 'CD' } # CD-Levy
-    elsif ($an =~ /^ka/i)   { return 'KA' } # Kasetti
-    elsif ($an =~ /^dvd/i)  { return 'DV' } # DVD-levy
-    elsif ($an =~ /^es/i)   { return 'ES' } # Esine
-    elsif ($an =~ /^(?:har|hv|j|kl|kmo|kon)/i) { return 'ES' }
-    elsif ($an =~ /^Kansio/i) { return 'KN' } # Kansio
-    elsif ($an =~ /^ki/i)   { return 'KI' } # Kirja
-    elsif ($an =~ /^lp/i)   { return 'LP' } # LP:t viedään Kohaan tutkimista varten, tuotannossa poistetaan
-    elsif ($an =~ /^opin/i) { return 'OP' } # Opinnäytetyö
+    if    ($an =~ /^nu/i)   { $it = 'NU' } # Nuotti
+    elsif ($an =~ /^cd/i)   { $it = 'CD' } # CD-Levy
+    elsif ($an =~ /^ka/i)   { $it = 'KA' } # Kasetti
+    elsif ($an =~ /^dvd/i)  { $it = 'DV' } # DVD-levy
+    elsif ($an =~ /^es/i)   { $it = 'ES' } # Esine
+    elsif ($an =~ /^(?:har|hv|j|kl|kmo|kon)/i) { $it = 'ES' }
+    elsif ($an =~ /^Kansio/i) { $it = 'KN' } # Kansio
+    elsif ($an =~ /^ki/i)   { $it = 'KI' } # Kirja
+    elsif ($an =~ /^lp/i)   { $it = 'LP' } # LP:t viedään Kohaan tutkimista varten, tuotannossa poistetaan
+    elsif ($an =~ /^opin/i) { $it = 'OP' } # Opinnäytetyö
     elsif ($an =~ /^vid/i)  { MMT::Exception::Delete::Silently->throw($s->logId()."' Video-items are silently set to die"); } # Videot poistetaan
 
   }
+
+  unless ($it) {
+
+    $it = $tableParams->[0];
+
+  }
+
+  return $it if $it;
+
   $log->warn($kohaObject->logId()." has unknown item type! Cannot parse AcqNum='$an'.");
+
+  return undef;
 }
 
 return 1;
