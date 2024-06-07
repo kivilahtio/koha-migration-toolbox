cb547ac9fe25a55256079e5e17ea34e0eaafa3ff
Lari Taskula <lari.taskula@hypernova.fi>
2024-06-07 12:49:27 +0300

    loader/postprocessing.sql: Remove unbound author from additional contents

--- loader/postprocessing.sql
+++ loader/postprocessing.sql
@@ -2,4 +2,6 @@
 --DELETE b FROM borrowers b WHERE b.borrowernumber NOT IN (SELECT borrowernumber FROM issues i WHERE b.borrowernumber=i.borrowernumber) AND b.borrowernumber NOT IN (SELECT borrowernumber FROM reserves r WHERE b.borrowernumber = r.borrowernumber);
 --
 
+UPDATE additional_contents SET borrowernumber=NULL WHERE borrowernumber NOT IN (SELECT borrowernumber FROM borrowers);
+
 SELECT 'postprocessing steps done';
